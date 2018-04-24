6e9ac7302d87dbb9f2eff1da7eccc880220726b0
Alejo <Alejo@Alejos-MacBook-Pro.local>
2018-04-24 19:08:36 +0200

    add icons and link to icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,8 +3,16 @@ import React from 'react';
 
 const Task = (props) => {
         return (
-            
-           <li className="collection-item">{props.task}</li>
+           <li className="collection-item">{props.task}
+                <a 
+                class="waves-effect waves-light btn secondary-content">
+                    <i class="large material-icons">done</i>
+                </a>
+                <a 
+                class="waves-effect waves-light btn secondary-content">
+                    <i class="large material-icons">close</i>
+                </a>
+            </li>
         )
 }
 
