02000975aafc6eb40254065ad9c6e5d8b626a79e
Alejo <Alejo@Alejos-MacBook-Pro.local>
2018-04-24 18:07:08 +0200

    changed names clean input field create task object

--- src/App.js
+++ src/App.js
@@ -3,32 +3,33 @@ import SearchBar from './components/SearchBar'
 
 
 class App extends Component {
-  state = {
-    presentTask: '',
-    tasksList: []
-  }
+    state = {
+        presentTask: '',
+        tasksList: []
+    }
 
-  addTaskToState = task => {
-    this.setState({presentTask: task.target.value })
-  }
+    addTask = task => {
+        this.setState({ presentTask: task.target.value })
+    }
 
-  pushTaksToTaskArray = () => {
-    const tasksList = [...this.state.tasksList];
-    tasksList.push(this.state.presentTask);
-    this.setState( { tasksList: tasksList, presentTask: '' });
-    
+    pushTask = () => {
+        const newTask = {
+            task: this.state.presentTask,
+            id: Date.now()
+        }
+        const tasksList = [...this.state.tasksList];
+        tasksList.push(newTask);
+        this.setState({ tasksList: tasksList, presentTask: '' });
+    }
 
-
-  } 
-   
-  render() {
-    return (
-      <div>
-       <h2>Todos</h2>
-       <SearchBar addTaskToState={this.addTaskToState} pushTaksToTaskArray={this.pushTaksToTaskArray}  />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h2>Todos</h2>
+                <SearchBar addTask={this.addTask} pushTask={this.pushTask} presentTask={this.state.presentTask} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,17 +4,18 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
 
     handleChange = (e) => {
-        this.props.addTaskToState(e);
+        this.props.addTask(e);
     }
 
     handleClick = () => {
-        this.props.pushTaksToTaskArray();
+        this.props.pushTask();
     }
-    render(){
+
+    render() {
         return (
             <div className="row">
                 <div className="input-field col s6">
-                    <input onChange={this.handleChange}  id="first_name2" type="text"/>
+                    <input onChange={this.handleChange} value={this.props.presentTask} id="first_name2" type="text" />
                     <label className="active" htmlFor="first_name2">First Name</label>
                 </div>
                 <a onClick={this.handleClick} className="col s1 waves-effect waves-light btn-small">Button</a>
