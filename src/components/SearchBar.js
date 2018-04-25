2ee55f1ba88e4fe8362d91a57f85908deb065573
Alejo <Alejo@Alejos-MacBook-Pro.local>
2018-04-25 07:19:03 +0200

    focus searchbar after submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,6 +10,7 @@ class SearchBar extends Component {
     handleClick = (e) => {
         e.preventDefault()
         this.props.pushTask();
+        this.textInput.focus();
     }
 
     render() {
@@ -17,7 +18,7 @@ class SearchBar extends Component {
             <form className="col s12" onSubmit={this.handleClick}>
                 <div className="row">
                     <div className="input-field col s10">
-                        <input onChange={this.handleChange} value={this.props.presentTask} id="first_name2" type="text" />
+                        <input ref={(input) => { this.textInput = input; }} onChange={this.handleChange} value={this.props.presentTask} id="first_name2" type="text" />
                         <label className="active" htmlFor="first_name2">First Name</label>
                     </div>
                     <a onClick={this.handleClick} className="col s2 waves-effect waves-light btn-small">Add task</a>
